a21cf830c2ec78475ac58cd93918a15ff05052d0
Artem Kravtsov <lordartemio02@gmail.com>
2022-11-16 19:08:34 +0300

    add IContract

---
+++ src/interfaces/Contract.interface.ts
@@ -0,0 +1,7 @@
+import { Market } from "./Market.interface";
+import { Markets } from "./Markets.interface";
+
+export interface IContract {
+  markets: (props: {}) => Markets[];
+  view_market: (props: { market_id: string }) => Market;
+}
